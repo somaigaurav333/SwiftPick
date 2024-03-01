204f32726d4d2f99762d8926d331d5ffadf66196
Gaurav <somaigaurav@gmail.com>
2024-03-02 01:21:44 +0530

    resolved merge conflicts

--- frontend/src/Components/RequestCard.js
+++ frontend/src/Components/RequestCard.js
@@ -3,23 +3,74 @@ import "./RequestCard.css";
 
 function RequestCard({ request }) {
   return (
-    <div className="RequestCard">
+    // <div className=" RequestCard shadow-md">
+    //   <div className="Head">
+    //     <div className="Username">{request.requesterUsername} </div>
+    //     <div className="Phone">{request.phoneNumber} </div>
+    //   </div>
+    //   <div className="DropLocation">{request.deliveryLocation}</div>
+    //   {request.items.map((item) => {
+    //     return (
+    //       <div className="Items" key={item}>
+    //         {item}
+    //       </div>
+    //     );
+    //   })}
+    //   <div className="Footer">
+    //     <div className="Date">{request.date}</div>
+    //     <div className="Time">{request.time}</div>
+    //   </div>
+    // </div>
+
+    // <div className="card mb-3 shadow-md RequestCard" styles="max-width: 18rem;">
+    //   <div className="card-header">
+    //     <div className="Head">
+    //       <div className="Username">{request.requesterUsername} Agrawal</div>
+    //       <div className="Phone">{request.phoneNumber} </div>
+    //     </div>
+    //     <div className="DropLocation">To: {request.deliveryLocation}</div>
+    //   </div>
+    //   <div className="card-body">
+    //     <p className="card-text">
+    //       {request.items.map((item) => {
+    //         return (
+    //           <div className="Items" key={item}>
+    //             {item}
+    //           </div>
+    //         );
+    //       })}
+    //     </p>
+    //   </div>
+    //   <div className="Footer">
+    //     <div className="Date">{request.date}</div>
+    //     <div className="Time"> {request.time} </div>
+    //   </div>
+    // </div>
+    <span
+      href="#"
+      class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 RequestCard"
+    >
       <div className="Head">
-        <div className="Username">{request.requesterUsername} </div>
+        <div className="Username">{request.requesterUsername} Agrawal</div>
         <div className="Phone">{request.phoneNumber} </div>
       </div>
-      <div className="DropLocation">{request.deliveryLocation}</div>
-      {request.items.map((item) => {
-        return (
-          <div className="Items" key={item}>
-            {item}
-          </div>
-        );
-      })}
-      <div className="Time">
-        {request.date} {request.time}
+      <div className="DropLocation">To: {request.deliveryLocation}</div>
+      <div className="card-body">
+        <p className="card-text">
+          {request.items.map((item) => {
+            return (
+              <div className="Items" key={item}>
+                {item}
+              </div>
+            );
+          })}
+        </p>
       </div>
-    </div>
+      <div className="Footer">
+        <div className="Date">{request.date}</div>
+        <div className="Time"> {request.time} </div>
+      </div>
+    </span>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import { Provider } from 'react-redux';
 import {store} from "./store"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
