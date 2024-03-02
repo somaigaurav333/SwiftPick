28e758aeade3588c299a422a5f6b5de05be2485e
Kush <kushpagrawal@gmail.com>
2024-03-02 20:47:56 +0530

    created both cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
-import express, { response } from "express";
-import mongoose from "mongoose";
-import { Location } from "./models/locationModel.js";
-import { Request } from "./models/requestModel.js";
-import locationRoute from "./routes/locationRoute.js";
-import requestRoute from "./routes/requestRoute.js";
-import userRoute from "./routes/userRoute.js";
-import { config } from "dotenv";
-import cors from "cors";
+import express, { response } from 'express';
+import mongoose from 'mongoose';
+import { Location } from './models/locationModel.js';
+import { Request } from './models/requestModel.js';
+import locationRoute from './routes/locationRoute.js';
+import requestRoute from './routes/requestRoute.js';
+import userRoute from './routes/userRoute.js';
+import { config } from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -19,11 +19,20 @@ config();
 //Middleware for handling CORS Policy
 //Option 1: Allow All origins with default of cors(*)
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'https://swift-pick-frontend.vercel.app',
   optionsSuccessStatus: 200,
-  credentials: true
-}
+  credentials: true,
+};
 app.use(cors(corsOptions));
+
+//
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200,
+//   credentials: true,
+// };
+// app.use(cors(corsOptions));
+//
 //Option 2: Allow Coustom Origin
 // app.use(
 //   cors({
@@ -33,20 +42,19 @@ app.use(cors(corsOptions));
 //   })
 // );
 
-app.get("/", (requset, response) => {
+app.get('/', (requset, response) => {
   // console.log(requset);
-  return response.status(234).send("Welcome");
+  return response.status(234).send('Welcome');
 });
 
-
-app.use("/locations", locationRoute);
-app.use("/requests", requestRoute);
-app.use("/auth", userRoute);
+app.use('/locations', locationRoute);
+app.use('/requests', requestRoute);
+app.use('/auth', userRoute);
 
 mongoose
   .connect(process.env.mongoDBURL)
   .then(() => {
-    console.log("App connected to location database");
+    console.log('App connected to location database');
     app.listen(process.env.PORT, () => {
       console.log(`App is listening on port: ${process.env.PORT}`);
     });
--- backend/models/requestModel.js
+++ backend/models/requestModel.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const requestSchema = mongoose.Schema({
   requesterId: {
@@ -54,4 +54,4 @@ const requestSchema = mongoose.Schema({
   },
 });
 
-export const Request = mongoose.model("Request", requestSchema);
+export const Request = mongoose.model('Request', requestSchema);
