8efbd918fadef402578ecc4e16f3ca6a422a0550
Gaurav <somaigaurav@gmail.com>
2024-04-23 22:59:25 +0530

    changed from localhost

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axios_instance = axios.create({
-  // baseURL: "https://swiftpick-2dm0.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://swiftpick-2dm0.onrender.com",
+  // baseURL: "http://localhost:5000",
 });
 
 export default axios_instance;
